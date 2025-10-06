adb13b43e9e618e186261497ed3cc59505ae7dc5
Ethan-Choy <ethan.choy@stu.ocsb.ca>
2025-10-06 13:14:04 -0400

    init

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ethan Choy
+ * Created on: Sep 2025
+ * This program shows the temperature
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// show temperature
+let currentTemperature = input.temperature();
+
+input.onButtonPressed(Button.A, function () {
+
+    // read temperature
+    basic.showString('The temperature is:')
+
+    // display on LED screen
+    basic.showNumber(currentTemperature)
+    basic.showString('C.')
+})
